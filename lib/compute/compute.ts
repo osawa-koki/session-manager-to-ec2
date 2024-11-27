08f74f03048193a07428afa21779b1bb212d0798
osawa-koki <koki@osawa.dev>
2024-11-27 11:27:55 +0900

    EC2のロールの設定。

--- lib/compute/compute.ts
+++ lib/compute/compute.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 interface ComputeStackProps extends cdk.StackProps {
@@ -25,6 +26,12 @@ export default class ComputeStack extends cdk.Stack {
       },
       instanceType: new ec2.InstanceType(process.env.EC2_INSTANCE_TYPE!),
       machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      role: new iam.Role(this, 'EC2Role', {
+        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'),
+        ],
+      }),
     });
 
     this.instance = instance;
